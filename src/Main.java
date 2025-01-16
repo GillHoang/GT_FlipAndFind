ba7f9846fea955d358934801c25069f9eb81856c
Hoang Hai Anh <89916615+GillHoang@users.noreply.github.com>
2025-01-16 09:49:57 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,27 @@
-
 import java.awt.CardLayout;
 import javax.swing.*;
 
 public class Main extends JFrame {
 	JPanel cardPanel;
 	CardLayout cardLayout;
-    public Main() {
-        super("Flip and Find");
+
+	public Main() {
+		super("Flip and Find");
 
 		cardPanel = new JPanel();
 		cardLayout = new CardLayout();
 		cardPanel.setLayout(cardLayout);
-		
-		cardPanel.add(new LoginPanel(), "login");
-		
-		this.add(cardPanel);
-        
-		this.setSize(1200, 700);
-        this.setResizable(false);
-        this.setVisible(true);
-    }
 
-    public static void main(String[] args) {
-        new Main();
-    }
+		cardPanel.add(new LoginPanel(cardLayout, cardPanel), "login");
+
+		this.add(cardPanel);
+
+		this.setSize(1200, 700);
+		this.setResizable(false);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
 }
